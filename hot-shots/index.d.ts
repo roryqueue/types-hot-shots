33d748dc9d37040949836af76a094e152a1e75ea
Rory Quinlan <roryquinlan@Maxwells-MacBook-Pro-4.local>
2017-05-01 22:32:24 -0400

    initial format for hotshots typedef, incomplete

---
+++ hot-shots/index.d.ts
@@ -0,0 +1,39 @@
+// Type definitions for hot-shots 4.4
+// Project: https://github.com/brightcove/hot-shots
+// Definitions by: Rory Quinlan <https://github.com/roryqueue>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = Client;
+
+interface SendMetric {
+    (
+      stat: string|string[],
+      value: number,
+      sampleRate?: number,
+      tags?: string[],
+      callback?: (error: Error|null, sentBytes: number) => void 
+    ): void;
+}
+
+declare class Client {
+    constructor(
+        host: string,
+        port: string|number,
+        prefix?: string,
+        suffix?: string,
+        globalize?: boolean,
+        cacheDns?: boolean,
+        mock?: boolean,
+        globalTags?: string[],
+        errorHandler?: (err: Error) => void,
+        maxBufferSize?: number,
+        bufferFlushInterval?: number,
+        sampleRate?: number
+    );
+
+    static timing: SendMetric;
+    static increment: SendMetric;
+    static decrement: SendMetric;
+    static gauge: SendMetric;
+    static histogram: SendMetric;
+}
