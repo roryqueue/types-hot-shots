4bc574c6e817c3a44d9aa29aa83a923cce75c02c
Rory Quinlan <roryquinlan@Maxwells-MacBook-Pro-4.local>
2017-05-01 22:46:42 -0400

    complete metrics, separate out timing since it uses a different term

--- hot-shots/index.d.ts
+++ hot-shots/index.d.ts
@@ -31,9 +31,18 @@ declare class Client {
         sampleRate?: number
     );
 
-    static timing: SendMetric;
+    timing(
+      stat: string|string[],
+      time: number,
+      sampleRate?: number,
+      tags?: string[],
+      callback?: (error: Error|null, sentBytes: number) => void 
+    ): void;
+
     static increment: SendMetric;
     static decrement: SendMetric;
     static gauge: SendMetric;
     static histogram: SendMetric;
+    static unique: SendMetric;
+    static set: SendMetric;
 }
