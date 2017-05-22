b8d53593026e01c52ff83bc5f71186b3ad1ba413
Rory Quinlan <rjamesquinlan@gmail.com>
2017-05-22 19:32:07 -0400

    add check, event, sendAll methods, generalize callback

--- hot-shots/index.d.ts
+++ hot-shots/index.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Rory Quinlan <https://github.com/roryqueue>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export = Client;
+export = HotShotClient;
 
 interface SendMetric {
     (
@@ -11,38 +11,76 @@ interface SendMetric {
       value: number,
       sampleRate?: number,
       tags?: string[],
-      callback?: (error: Error|null, sentBytes: number) => void 
+      callback?: ErrorOrBytesCallback
     ): void;
 }
 
-declare class Client {
-    constructor(
-        host: string,
-        port: string|number,
-        prefix?: string,
-        suffix?: string,
-        globalize?: boolean,
-        cacheDns?: boolean,
-        mock?: boolean,
-        globalTags?: string[],
-        errorHandler?: (err: Error) => void,
-        maxBufferSize?: number,
-        bufferFlushInterval?: number,
-        sampleRate?: number
-    );
+type ErrorOrBytesCallback = (error?: Error|null, sentBytes?: number) => void 
 
-    timing(
-      stat: string|string[],
-      time: number,
-      sampleRate?: number,
-      tags?: string[],
-      callback?: (error: Error|null, sentBytes: number) => void 
-    ): void;
 
-    static increment: SendMetric;
-    static decrement: SendMetric;
-    static gauge: SendMetric;
-    static histogram: SendMetric;
-    static unique: SendMetric;
-    static set: SendMetric;
+declare class HotShotClient {
+  constructor(
+      host: string,
+      port: string|number,
+      prefix?: string,
+      suffix?: string,
+      globalize?: boolean,
+      cacheDns?: boolean,
+      mock?: boolean,
+      globalTags?: string[],
+      errorHandler?: (err: Error) => void,
+      maxBufferSize?: number,
+      bufferFlushInterval?: number,
+      sampleRate?: number
+  );
+
+  timing(
+    stat: string|string[],
+    time: number,
+    sampleRate?: number,
+    tags?: string[],
+    callback?: ErrorOrBytesCallback
+  ): void;
+
+  static increment: SendMetric;
+  static decrement: SendMetric;
+  static gauge: SendMetric;
+  static histogram: SendMetric;
+  static unique: SendMetric;
+  static set: SendMetric;
+
+  check(
+    name: string,
+    status: 0 | 1 | 2 | 3,
+    options?: {
+      date_happened?: Date,
+      hostname?: string,
+      message?: string
+    },
+    tags?: ReadonlyArray<string> | ErrorOrBytesCallback,
+    callback?: ErrorOrBytesCallback
+  ): void
+
+  event(
+    title: string,
+    text?: string,
+    options?: {
+      date_happened?: Date,
+      hostname?: string,
+      aggregation_key?: string,
+      priority?: 'normal' | 'low',
+      source_type_name?: string,
+      alert_type?: 'error' | 'warning' | 'info' | 'success',
+    },
+    tags?: ReadonlyArray<string> | ErrorOrBytesCallback,
+    callback?: ErrorOrBytesCallback
+  ): void
+
+  sendAll(
+    stat: string | ReadonlyArray<string>,
+    value: number,
+    sampleRate?: number,
+    tag?: ReadonlyArray<string>,
+    callback?: ErrorOrBytesCallback
+  ): void
 }
